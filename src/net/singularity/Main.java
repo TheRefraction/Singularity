18a91386e62d40b1578927ff05ed7323d7ac4b8f
TheRefraction <the.r3fraction@gmail.com>
2024-03-23 21:15:04 +0100

    Window class created

--- src/net/singularity/Main.java
+++ src/net/singularity/Main.java
@@ -1,4 +1,18 @@
 package net.singularity;
 
+import net.singularity.system.Window;
+import org.lwjgl.Version;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(Version.getVersion());
+        Window window = new Window("Singularity", 800, 600, false);
+        window.init();
+
+        while(!window.windowShouldClose()) {
+            window.update();
+        }
+
+        window.cleanup();
+    }
 }
